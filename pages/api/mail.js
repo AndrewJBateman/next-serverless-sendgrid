18b85f9b045351e53b379328ce4a705156c7fc52
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-28 17:45:47 +0200

    feat: add code to send form data via email

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,9 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mail = require('@sendgrid/mail')
 
- const mailMessage = (req, res) => {
+mail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const mailMessage = (req, res) => {
   const body = JSON.parse(req.body);
-  console.log('body: ', body);
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
+
+  const data = {
+    to: 'gomezbateman@yahoo.com',
+    from: 'hello@andrewbateman.org',
+    subject: 'New web form message',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>')
+  }
+
+  mail.send(data);
+
   res.status(200).json({ status: 'Ok' });
 }
+
+// For sent acknowledgement: use the useState hook and set a "sent" state after the request finishes, 
+// then use to control what's displayed on the page
 
 export default mailMessage;
